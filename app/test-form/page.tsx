258d29880de1af46e69129534294ac9815d5684f
Pratap Vaja <pratapvaja4@gmail.com>
2026-04-26 10:07:55 -0700

    Proper final supabase path fix

--- app/test-form/page.tsx
+++ app/test-form/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { supabase } from "../../../supabase";
+import { supabase } from "../../supabase";
 
 export default function InquiryFormPage() {
   const [form, setForm] = useState({
